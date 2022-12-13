9c6a2b3346fa385963fac060150a7d15ae04ac0f
DELL <DELL@DESKTOP-7C4357L>
2022-12-13 00:25:58 -0600

    Haeder footer components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Pgaes/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import {BroweserRouter as Router,Route,Switch} from 'react-router-dom';
 
 
 function App() {
   return (
-    <Home/>
+
+    <div>
+<route>
+    <Header/>
+<switch>
+
+    <Route exact path='/'component={Home} />
+    <Route path='/About'component={About} />
+    <Route path='/Contact'component={Contact} />
+    <Route path='/Login'component={Login} />
+    <Route path='/Signup'component={Signup} />
+    <Route path='/Loadboard'component={Loadboard} />
+    </switch>
+    <Footer/>  
+    </route>
+    </div>
+    
   );
 }
 
