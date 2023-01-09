58263fa539be23c75d07b6f438bebb9bbd880bf8
DELL <DELL@DESKTOP-7C4357L>
2023-01-09 06:02:51 -0600

    Updated Home Page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -10,14 +10,16 @@ class Contact extends React.Component{
                 <h2>Contact</h2>
                 <p>Contact Us</p>
             </div><div>
+                
                     <iframe style={{ border: 0, width: '100%', height: '270px' }} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameBorder={0} allowFullScreen />
+                
                 </div><div className="row mt-5">
                     <div className="col-lg-4">
                         <div className="info">
                             <div className="address">
                                 <i className="bi bi-geo-alt" />
                                 <h4>Location:</h4>
-                                <p>A108 Adam Street, New York, NY 535022</p>
+                                <p>Gulshan Iqbal Karachi, Pakistan 75300</p>
                             </div>
                             <div className="email">
                                 <i className="bi bi-envelope" />
